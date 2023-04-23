6f332ee344b32530d22232d9b99b962e171e8582
Holso <109223950+Holso@users.noreply.github.com>
2023-04-23 22:25:33 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -36,49 +36,40 @@ function clearAll(){
 operator = 0, nrOperatori = 1;
 function operatii(identifica){
     if(numarDeCifre > 0){
-        if(verifica == 0)
-            operator ++;
-        if(operator > 1 && verifica == 0){
-            sir = ecranPrincipal.innerHTML;
-            ultimaLitera = sir.charAt(sir.length - 1);
-            ultimaLitera = 2;
-            if(!['+', '-', '*', '/'].includes(ultimaLitera)  && punct == 0){
-                ecranPrincipal.innerHTML = ecranSecundar.innerText;
-                ecranSecundar.innerHTML = ""; 
-            }else if(punct == 0){
-                ecranPrincipal.innerHTML = sir.slice(0, sir.length - 1);
-                console.log(1);
-            }
-            if(punct == 1 && !ultimaLitera.match(/[0-9]/)){
-                identifica = -1;
-                punctEgal = -1;    
-            }else if(punct == 1){
-                punctEgal = 1;
-                console.log("da");
-            }
-        }
         if(verifica == 0){
             if(identifica == 1){ //inmultire
                 ecranPrincipal.innerHTML = ecranPrincipal.innerHTML + "*";
             }else if(identifica == 2){ //impartire
                 ecranPrincipal.innerHTML = ecranPrincipal.innerHTML + "/";
             }else if(identifica == 3){ //adunare
-                console.log(ecranPrincipal.innerHTML);
                 ecranPrincipal.innerHTML = ecranPrincipal.innerHTML + "+";
             }else if(identifica == 4){ //scadere
                 ecranPrincipal.innerHTML = ecranPrincipal.innerHTML + "-";
             }else if(identifica == 5){//procent
                 ecranPrincipal.innerHTML = ecranPrincipal.innerHTML + "%";
-            }else if(identifica == 6 && verifica == 0){//comparare
+            }else if(identifica == 6){//comparare
                 a = ecranPrincipal.innerHTML;
-                ecranPrincipal.innerHTML = ecranPrincipal.innerHTML + " Si ";
+                ecranPrincipal.innerHTML = eval(ecranPrincipal.innerHTML) + " Si ";
+                ecranSecundar.innerHTML = "";
                 verifica = 1;
             }else if(identifica == 7){
                 punct = 1;punctEgal = -1;
                 ecranPrincipal.innerHTML = ecranPrincipal.innerHTML + ".";
             }
         }
-    }else if(identifica == 4){
+        if(verifica == 0 && punct == 0){
+            operator ++;
+            sir = ecranPrincipal.innerHTML;
+            ultimaLitera = sir.charAt(sir.length - 1);
+            console.log(sir);
+            if(['+', '-', '*', '/'].includes(ultimaLitera) && operator > 1){
+                ecranPrincipal.innerHTML = ecranSecundar.innerText + ultimaLitera;
+                ecranSecundar.innerHTML = "";
+            }
+        }
+ 
+
+    }else if(identifica == 4){//scadere
         ecranPrincipal.innerHTML = ecranPrincipal.innerHTML + "-";
     }
     
@@ -90,6 +81,7 @@ function egal(){
         ecranSecundar.innerHTML = "";
         nrOperatori = 1;
     }else if(verifica == 1){
+        console.log(-1012010201);
         if(a > b){
             ecranPrincipal.innerHTML = a + " > " + b;
         }else if(a < b){
@@ -99,6 +91,9 @@ function egal(){
         }
     }
     
+
+
+    
 }
 
         /*==== X si 0 ====*/
@@ -106,6 +101,7 @@ function egal(){
 //====Declarare====
 let urmatorul,numarBoxFilled,castig, numarDinMatrice,scoro,scorx,i,j;
 let box = document.querySelector(".box");
+let backgroundBox = document.querySelector(".xsio");
 urmatorul = 1, numarBoxFilled = scoro = scorx = 0;
 
 let matrice = [
@@ -114,7 +110,7 @@ let matrice = [
     [0, 0, 0]
 ];
 
-castig = 0, numarDinMatrice = 0, scor=0;
+castig = 0, numarDinMatrice = 0;
 function patrat(linie,coloana){
     numarBoxFilled ++;
     box = document.querySelector(".box" + linie + coloana);
@@ -128,6 +124,7 @@ function patrat(linie,coloana){
         matrice[linie][coloana] = 2;
     }else if(matrice[linie][coloana] != 0 ){
         alert("Nu  poti pune unde a fost pus deja" );
+        numarBoxFilled -- ;
     }
     if(numarBoxFilled >= 3){
         if(matrice[0][0] == matrice[1][1] && matrice[2][2] == matrice[1][1] && matrice[1][1] != 0){//verifici diag principala
@@ -157,14 +154,12 @@ function patrat(linie,coloana){
         }
         if(numarBoxFilled == 9 && castig == 0){
             setTimeout(function() {
-                for(i=0;i<3;i++){
-                    for(j=0;j<3;j++){
-                        matrice[i][j] = 0;
-                        document.querySelector(".box" + i + j).innerHTML = "";
-                    }
-                }
+                backgroundBox.style.background = "red";
             }, 100);
-            
+            setTimeout(function() {
+                backgroundBox.style.background = "#e8e8e8";
+                continueGame();
+            }, 1500);
         }
         if(castig == 1){
             if(numarDinMatrice == 1){
@@ -179,16 +174,20 @@ function patrat(linie,coloana){
 
             setTimeout(function() {
                 document.querySelector(".boxScor").style.background = "#228B22";
-            }, 100);
+            }, 200);
             setTimeout(function() {
                 document.querySelector(".boxScor").style.background = "transparent";
-              }, 1500);
+            }, 1500);
+
         }
     }
+    
+
 }
 
 function continueGame(){
     urmatorul = 1;
+    numarBoxFilled = 0;
     for(i=0;i<3;i++){
         for(j=0;j<3;j++){
             matrice[i][j] = 0;
@@ -199,8 +198,34 @@ function continueGame(){
 
 function restartGame(){
     scoro = 0;
-    scorx = 0
+    scorx = 0;
+    numarBoxFilled = 0;
     document.getElementById("scoro").innerHTML = "";
     document.getElementById("scorx").innerHTML = "";
     continueGame();
 }
+
+
+
+/*if(verifica == 0)
+            operator ++;
+        if(operator > 1 && verifica == 0){
+            sir = ecranPrincipal.innerHTML;
+            ultimaLitera = sir.charAt(sir.length - 1);
+            if(['+', '-', '*', '/'].includes(ultimaLitera)  && punct == 0){
+                ecranPrincipal.innerHTML = ecranSecundar.innerText + ultimaLitera;
+                ecranSecundar.innerHTML = ""; 
+            }else if(punct == 0){
+                ecranPrincipal.innerHTML = sir.slice(0, sir.length - 1);
+                console.log(1);
+            }
+            if(punct == 1 && !ultimaLitera.match(/[0-9]/)){
+                identifica = -1;
+                punctEgal = -1;    
+            }else if(punct == 1){
+                punctEgal = 1;
+                console.log("da");
+            }
+        }else if(operator == 0){
+            console.log("operator 0")
+        }*/
